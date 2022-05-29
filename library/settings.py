5325bdf6bbf476e0542fd2049cee3422210675d7
nijanthan <nijanthan.rajachandran@impigertech.com>
2022-05-29 07:57:13 +0530

    added database connection

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qxwwkl=i(p3gg+o1)yr!5to=@r^em8%3mces_)xa1!=e31ph5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library_manager',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'root'
     }
 }
 
